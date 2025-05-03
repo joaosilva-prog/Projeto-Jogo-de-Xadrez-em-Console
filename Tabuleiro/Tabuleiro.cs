8a34d1eb0f9a18e433851d2bc79dc150e8c787a4
Joao Silva <sjoao5498@gmail.com>
2025-05-03 13:36:03 -0300

    Tratando erros com excecoes personalizadas de tabuleiro

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -8,7 +8,7 @@ namespace tabuleiro
         public int colunas { get; set; }
         private Peca[,] pecas;
 
-        public Tabuleiro( int linhas, int colunas )
+        public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
             this.colunas = colunas;
@@ -18,10 +18,40 @@ namespace tabuleiro
         {
             return pecas[linha, coluna];
         }
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool ExistePeca(Posicao pos)
+        {
+         ValidarPosicao(pos);
+            return peca(pos) != null;
+        }
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa Posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
