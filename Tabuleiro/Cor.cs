db7f86b9bc8311d53bfffa733462ba5aefdfc9da
Joao Silva <sjoao5498@gmail.com>
2025-05-03 11:47:43 -0300

    Comecando implementacao do tabuleiro

---
+++ Tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amaraela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
---
+++ Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            qteMovimentos = 0;
+            this.tabuleiro = tabuleiro;
+        }
+    }
+}
